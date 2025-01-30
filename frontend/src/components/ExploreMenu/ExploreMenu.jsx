16ec297058efa17f649f212bf6e60677bd229768
Aanchallll18 <kumariaachal002@gmail.com>
2025-01-30 14:58:00 +0530

    commit

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Exploremenu.css";
 import { menu_list } from "../../assets/assets";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category,setcategory}) => {
 	return (
 		<div className="explore-menu" id="explore-menu">
 			<h1>Explore Our Menu</h1>
@@ -15,13 +15,16 @@ const ExploreMenu = () => {
 			<div className="explore-menu-list">
 				{menu_list.map((item, index) => {
 					return (
-                    <div key={index}className="explore-menu-list-item">
-                       <img src={item.menu_image} alt="" /> 
+                    <div onClick={()=>setcategory(prev===item.menu_name ? "All" : item.menu_name)} 
+					key={index}className="explore-menu-list-item">
+                       <img className={category === item.menu_name ? "active" : ""}
+					   src={item.menu_image} alt="" /> 
                        <p>{item.menu_name}</p>
                     </div>
                     )
 				})}
 			</div>
+			<hr />
 		</div>
 	);
 };
