41f64f851d661b9db4678a2a8349bda312f6659e
Aanchallll18 <kumariaachal002@gmail.com>
2025-02-18 20:18:09 +0530

    commit

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../../assets/assets'
 
-const Login = () => {
+const Login = ({setShowLogin}) => {
+  const [currState,setCurrState]=useState("Login")
   return (
     <div>
-      
+      <form action="" className="login-container">
+        <div className="login-popup-title">
+          <h2>{currState}</h2>
+          <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+        </div>
+        <div className="login-input">
+          {currState ==='Login'?<></>:<input type="text" required placeholder='Your Name'/>}
+          <input type="email" required placeholder='Your email'/>
+          <input type="password" required placeholder='Password'/>
+
+        </div>
+        <button>
+          {currState === "Sign Up" ? "Create account":"Login"}
+        </button>
+        <div className="login-condition">
+          <input type="checkbox" name="" id="" required/>
+          <p>By continuing,i agree tothe terms of use & privacy policy</p>
+        </div>
+      </form>
     </div>
   )
 }
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import {assets} from '../../assets/assets'
 import { Link } from 'react-router-dom'
 
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
   const [menu,setMenu]=useState("home")
   return (
     <div className='navbar'>
@@ -29,7 +29,7 @@ const Navbar = () => {
          <div className='dot'></div>
          
         </div>
-        <button>
+        <button onClick={()=>setShowLogin(true)}>
           Sign in
         </button>
       </div>
