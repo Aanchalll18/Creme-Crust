8a84408138ec24d847ae53fcc6817d7449fc8335
Aanchallll18 <kumariaachal002@gmail.com>
2025-02-18 21:00:59 +0530

    commit

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,31 +1,52 @@
-import React, { useState } from 'react'
-import { assets } from '../../assets/assets'
+import React, { useState } from "react";
+import { assets } from "../../assets/assets";
+import './Login.css'
 
-const Login = ({setShowLogin}) => {
-  const [currState,setCurrState]=useState("Login")
-  return (
-    <div>
-      <form action="" className="login-container">
-        <div className="login-popup-title">
-          <h2>{currState}</h2>
-          <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
-        </div>
-        <div className="login-input">
-          {currState ==='Login'?<></>:<input type="text" required placeholder='Your Name'/>}
-          <input type="email" required placeholder='Your email'/>
-          <input type="password" required placeholder='Password'/>
+const Login = ({ setShowLogin }) => {
+	const [currState, setCurrState] = useState("Login");
+	return (
+		<div className="login">
 
-        </div>
-        <button>
-          {currState === "Sign Up" ? "Create account":"Login"}
-        </button>
-        <div className="login-condition">
-          <input type="checkbox" name="" id="" required/>
-          <p>By continuing,i agree tothe terms of use & privacy policy</p>
-        </div>
-      </form>
-    </div>
-  )
-}
+			<form action="" className="login-container">
+				
+					<div className="login-title">
+						<h2>{currState}</h2>
+						<img
+							onClick={() => setShowLogin(false)}
+							src={assets.cross_icon}
+							alt=""
+						/>
+					
+				</div>
 
-export default Login
+				<div className="login-input">
+					{currState === "Login" ? (
+						<></>
+					) : (
+						<input type="text" required placeholder="Your Name" />
+					)}
+					<input type="email" required placeholder="Your email" />
+					<input type="password" required placeholder="Password" />
+				</div>
+				<button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
+				<div className="login-condition">
+					<input type="checkbox" name="" id="" required />
+					<p>By continuing,i agree tothe terms of use & privacy policy</p>
+				</div>
+				{currState === "Login" ? (
+					<p>
+						Create a new account {" "}
+						<span onClick={() => setCurrState("Sign Up")}>Click here</span>
+					</p>
+				) : (
+					<p>
+						Already have an account?{" "}
+						<span onClick={() => setCurrState("Login")}>Login here</span>
+					</p>
+				)}
+			</form>
+		</div>
+	);
+};
+
+export default Login;
