279b2eb92def5c8e66e551a5c61782b3883fe76c
Aanchallll18 <kumariaachal002@gmail.com>
2025-02-20 18:48:05 +0530

    commit

--- backend/controllers/foodControllers.js
+++ backend/controllers/foodControllers.js
@@ -3,5 +3,23 @@ import fs from 'fs'
 
 const addFood =async(req,res) =>{
 
+    let image_filename=`${req.file.filename}`;
+
+    const food=new foodModel({
+        name:req.body.name,
+        description:req.body.description,
+        price:req.body.price,
+        category:req.body.category,
+        image:image_filename
+    })
+    try {
+        
+    } catch (error) {
+       console.log(error)
+       res.json({
+        success:false,
+        message:error.mess
+       }) 
+    }
 }
 export {addFood}
