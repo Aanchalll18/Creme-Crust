9f62c0ebb67bcd9a9c37296a536959bceb8d4ea1
Aanchallll18 <kumariaachal002@gmail.com>
2025-02-21 00:31:35 +0530

    commit

--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import './sidebar.css'
+import React from "react";
+import "./sidebar.css";
+import { assets } from "../../assets/assets";
 
 const sidebar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	return (
+		<div className="sidebar">
+			<div className="sidebar-options">
+				<div className="sidebar-option">
+					<img src={assets.add_icon} alt="" />
+					<p>Add Items</p>
+				</div>
+                <div className="sidebar-option">
+					<img src={assets.order_icon} alt="" />
+					<p>List Items</p>
+				</div>
+                <div className="sidebar-option">
+					<img src={assets.order_icon} alt="" />
+					<p>Orders</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default sidebar
+export default sidebar;
